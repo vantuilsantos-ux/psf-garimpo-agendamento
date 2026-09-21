79cf1c501b20937b8be61bbf4add33339ca31fd0
vantuilsantos-ux <vantuil.santos@ufms.br>
2026-09-21 06:55:36 -0300

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>PSF Garimpo de Capoeirana - Agendamento</h1>
+      <p>Plataforma de agendamento de consultas médicas.</p>
+    </div>
+  );
+}
+
+export default App;
